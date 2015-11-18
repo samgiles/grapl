095449b06962b257a84ebb3aeba6b1d1273f6a4b
Sam Giles <sam.e.giles@gmail.com>
2015-11-18 17:18:14 +0000

    Windows clean up

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,3 @@
-#ifdef WIN32
-#define WIN32_LEAN_AND_MEAN //remove MFC overhead from windows.h which can cause slowness
-#define WIN32_EXTRA_LEAN
-
-#include <windows.h>
-#endif // WIN32
-
 #include <iostream>
 #include <GLFW/glfw3.h>
 
