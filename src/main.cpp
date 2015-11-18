f11d48f2017bb8281ebc836e69ae01eb1d5bef45
Sam Giles <sam.e.giles@gmail.com>
2015-11-18 14:29:23 +0000

    Include glfw in the build \o/ wooh it works :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
-#include "gl.h"
+#ifdef WIN32
+#define WIN32_LEAN_AND_MEAN //remove MFC overhead from windows.h which can cause slowness
+#define WIN32_EXTRA_LEAN
 
-int main(int argc, char* argv[]) {
+#include <windows.h>
+#endif // WIN32
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
